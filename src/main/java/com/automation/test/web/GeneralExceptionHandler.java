f84107f680f696302b75dec4d883211d96ea706d
sergio.mendonca <stmfmendonca@rd.com.br>
2025-01-26 13:59:08 -0300

    teste integração - nomes repetidos.

--- src/main/java/com/automation/test/web/GeneralExceptionHandler.java
+++ src/main/java/com/automation/test/web/GeneralExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.automation.test.web;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -17,4 +19,14 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
 
         return super.handleMethodArgumentNotValid(ex, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
     }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    private ResponseEntity<Object> handleConflict(DataIntegrityViolationException ex){
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(ex.getMessage());
+    }
+
+
 }
--- src/test/java/com/automation/test/web/PlanetControllerTest.java
+++ src/test/java/com/automation/test/web/PlanetControllerTest.java
@@ -7,10 +7,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static com.automation.test.common.PlanetConstants.PLANET;
@@ -61,6 +63,18 @@ public class PlanetControllerTest {
                 .andExpect(status().isUnprocessableEntity());
     }
 
+    @Test
+    public void createPlanet_WithExistingName_ReturnsConflict() throws Exception {
+        when(planetService.create(any())).thenThrow(DataIntegrityViolationException.class);
 
+        mockMvc.perform(
+                        post("/planets")
+                                .content(objectMapper.writeValueAsString(PLANET))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict());
 
     }
+
+
+
+}
