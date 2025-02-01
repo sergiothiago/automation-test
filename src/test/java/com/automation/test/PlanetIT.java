042eea9bc6891964cc3185b4b73d029ac9c1b202
Sergio <stmfmendonca.radix@rd.com.br>
2025-02-01 20:05:03 -0300

    Teste E2E lista por id e lista todos

--- src/test/java/com/automation/test/PlanetIT.java
+++ src/test/java/com/automation/test/PlanetIT.java
@@ -2,6 +2,8 @@ package com.automation.test;
 
 import com.automation.test.domain.Planet;
 import org.assertj.core.api.Assertions;
+import org.h2.table.Plan;
+import org.hibernate.tool.schema.internal.exec.ScriptTargetOutputToFile;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,10 +13,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
-import static com.automation.test.common.PlanetConstants.PLANET;
+import java.util.Arrays;
+import java.util.List;
 
+import static com.automation.test.common.PlanetConstants.PLANET;
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"/import_planets.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 @Sql(scripts = {"/remove_planets.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_CLASS)
 public class PlanetIT {
 
@@ -41,4 +46,26 @@ public class PlanetIT {
 
     }
 
+    @Test
+    public void findByIdPlanet_ReturnsPlanet(){
+
+        //String url, Class<T> responseType, Object... urlVariables
+        ResponseEntity<Planet> sut =
+                testRestTemplate.getForEntity("/planets/1", Planet.class);
+
+        Assertions.assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(sut.getBody().getName()).isEqualTo("Endor");
+    }
+
+    @Test
+    public void list_Planets_ReturnsPlanets(){
+
+        ResponseEntity<Planet[]> sut =
+                testRestTemplate.getForEntity("/planets", Planet[].class);
+
+        Assertions.assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        Assertions.assertThat(sut.getBody()).isNotNull();
+        Assertions.assertThat(sut.getBody()).hasSize(4);
+    }
 }
---
+++ src/test/resources/import_planets.sql
@@ -0,0 +1,5 @@
+INSERT INTO planet (climate, terrain, name) VALUES
+('Tropical', 'Forest', 'Endor'),
+('Arid', 'Desert', 'Tatooine'),
+('Temperate', 'Grasslands, Mountains', 'Alderaan'),
+('Frozen', 'Tundra, Ice Caves', 'Hoth');
